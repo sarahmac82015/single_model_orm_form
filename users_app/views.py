a41870e8aa6ca65ad085ae2e18af82181af6f98f
sarahmac82015 <65735476+sarahmac82015@users.noreply.github.com>
2020-07-19 19:15:09 -0500

    first commit

---
+++ users_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from . models import User
+
+def index(request):
+    context={
+        'users':User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def create(request):
+    User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        age=request.POST['age'],
+        email=request.POST['email'],
+    )
+    return redirect('/')
+# Create your views here.
